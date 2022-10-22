a6f0345b522fa19c0a75e7197833d090ea1eb7e4
Hoàng Gia Trương Cao <truongcaohoanggia@gmail.com>
2022-10-22 17:18:36 +0700

    update

--- run.sh
+++ run.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
-python main.py level_1 1 bfs
-python main.py level_1 1 dfs
-python main.py level_1 1 ucs
-python main.py level_1 1 gbfs
-python main.py level_1 1 astar
+python ./source/main.py level_1 1 bfs
+python ./source/main.py level_1 1 dfs
+python ./source/main.py level_1 1 ucs
+python ./source/main.py level_1 1 gbfs
+python ./source/main.py level_1 1 astar
 #
-python main.py level_1 2 bfs
-python main.py level_1 2 dfs
-python main.py level_1 2 ucs
-python main.py level_1 2 gbfs
-python main.py level_1 2 astar
+python ./source/main.py level_1 2 bfs
+python ./source/main.py level_1 2 dfs
+python ./source/main.py level_1 2 ucs
+python ./source/main.py level_1 2 gbfs
+python ./source/main.py level_1 2 astar
 #
-python main.py level_1 3 bfs
-python main.py level_1 3 dfs
-python main.py level_1 3 ucs
-python main.py level_1 3 gbfs
-python main.py level_1 3 astar
+python ./source/main.py level_1 3 bfs
+python ./source/main.py level_1 3 dfs
+python ./source/main.py level_1 3 ucs
+python ./source/main.py level_1 3 gbfs
+python ./source/main.py level_1 3 astar
 #
-python main.py level_1 4 bfs
-python main.py level_1 4 dfs
-python main.py level_1 4 ucs
-python main.py level_1 4 gbfs
-python main.py level_1 4 astar
+python ./source/main.py level_1 4 bfs
+python ./source/main.py level_1 4 dfs
+python ./source/main.py level_1 4 ucs
+python ./source/main.py level_1 4 gbfs
+python ./source/main.py level_1 4 astar
 #
-python main.py level_1 5 bfs
-python main.py level_1 5 dfs
-python main.py level_1 5 ucs
-python main.py level_1 5 gbfs
-python main.py level_1 5 astar
+python ./source/main.py level_1 5 bfs
+python ./source/main.py level_1 5 dfs
+python ./source/main.py level_1 5 ucs
+python ./source/main.py level_1 5 gbfs
+python ./source/main.py level_1 5 astar
